40dc84db2d7def1609b8c7fbae8a51be28ee4f16
jesusbeb <jesusbelb@gmail.com>
2023-05-31 17:12:46 -0600

    Personalizando error 404 y 500

--- src/main/java/com/bolsadeideas/springboot/error/app/controllers/AppController.java
+++ src/main/java/com/bolsadeideas/springboot/error/app/controllers/AppController.java
@@ -1,11 +1,14 @@
 package com.bolsadeideas.springboot.error.app.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 @Controller
 public class AppController {
 
 	@GetMapping("/index")
 	public String index() {
-		
-		return index;
+		Integer valor = 100/0; //variable para forzar un error. Este error sucedera primero, por lo tanto no sucedera algun otro error que continue
+		return "index";
 	}
 }
